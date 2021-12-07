85d33bf6281eb0ab18a76629be96b8c45e0a41d2
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-12-07 21:58:39 +0600

    timestamp 2h

---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css'
+
+function Post({avatar, displayName, username, verified, message, attachment, timeStamp}) {
+    return (
+        <div className='post'>
+            
+        </div>
+    )
+}
+
+export default Post
